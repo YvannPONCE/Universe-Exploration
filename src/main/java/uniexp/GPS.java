c0949e379074dfb6ec68b2c556d0b61146390700
Yvann PONCE <yvann.ponce@gmail.com>
2023-05-19 10:23:47 +0200

    add gps architecture #4

---
+++ src/main/java/uniexp/GPS.java
@@ -0,0 +1,46 @@
+package uniexp;
+
+import uniexp.galaxy.Galaxy;
+
+import java.util.List;
+
+public class GPS {
+
+    //1 Recuperez la liste des planètes
+    //2 Trier ces planète par distance
+    //3 récupérer la planète contenant de l'eau la plus proche
+    //4 Former une galaxie constituée de planètes
+    //5 Trouver le chemin menant à la planète choisie
+
+    private List<Planete> planeteList;
+
+    //1
+    public GPS()
+    {
+        //planeteList = Database.getPlaneteList();
+    }
+
+    //2
+    private List<Planete> sortPlanetes()
+    {
+        return null;
+    }
+
+    //3
+    private Planete findPotentialPlanate()
+    {
+         return null;
+    }
+
+    //4
+    private Galaxy buildGalaxy()
+    {
+         return null;
+    }
+
+    //5
+    private List<Planete> findTrajectory()
+    {
+         return null;
+    }
+}
