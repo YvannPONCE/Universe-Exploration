7cfff946f0b30c1b749b595fee3973f0cc38d5fa
Yvann PONCE <yvann.ponce@gmail.com>
2023-05-20 20:36:07 +0200

    add data file

---
+++ src/main/java/uniexp/DataBase.java
@@ -0,0 +1,16 @@
+package uniexp;
+
+import java.util.List;
+
+public class DataBase {
+
+   public DataBase()
+   {
+
+   }
+
+   public List<Planete> getPlaneteList()
+   {
+       return null;
+   }
+}
