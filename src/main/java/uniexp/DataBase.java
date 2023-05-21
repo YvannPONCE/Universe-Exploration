66d4171c609c5ab7efef16b21509017186ac512e
Yvann PONCE <yvann.ponce@gmail.com>
2023-05-21 13:38:16 +0200

    clean imports and automatic cleans #33

--- src/main/java/uniexp/DataBase.java
+++ src/main/java/uniexp/DataBase.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static java.lang.Boolean.parseBoolean;
@@ -18,13 +19,21 @@ public class DataBase {
     }
 
     public List<Planete> getPlaneteList() {
-        loadData("src/main/resources/PlanetsName.txt");
+        try{
+            loadData("src/main/resources/PlanetsName.txt");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
         return planeteList;
     }
 
-    private void loadData(String path) {
+    private void loadData(String path) throws IOException {
+
+        BufferedReader reader = null;
         try {
-            BufferedReader reader;
             reader = new BufferedReader(new FileReader(path));
             String line = reader.readLine();
 
@@ -33,11 +42,17 @@ public class DataBase {
                 planeteList.add(new Planete(values[0], parseDouble(values[1]), parseBoolean(values[2]), values[3], parseDouble(values[4])));
                 line = reader.readLine();
             }
-
-            reader.close();
+            
         } catch (IOException e) {
             System.out.println("Wrong input format :\n");
             e.printStackTrace();
+            return;
+        }
+        finally {
+            if(reader != null)
+            {
+                reader.close();
+            }
         }
 
     }
